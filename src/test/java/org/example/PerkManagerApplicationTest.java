8cfbc75a0cde9433a913b8a3bb0208a0ea152a7f
BigMozz <armin.mozafari12@gmail.com>
2025-11-02 21:08:41 -0500

    CI/CD

---
+++ src/test/java/org/example/PerkManagerApplicationTest.java
@@ -0,0 +1,12 @@
+package org.example;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PerkManagerApplicationTest {
+    @Test
+    void contextLoads() {
+        // Test that Spring application starts
+    }
+}
