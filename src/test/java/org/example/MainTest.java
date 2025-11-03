33f4435f757d88809b177d5a82a4197c7bac8b1e
BigMozz <armin.mozafari12@gmail.com>
2025-11-02 21:46:20 -0500

    CI/CD

--- src/test/java/org/example/MainTest.java
+++ src/test/java/org/example/MainTest.java
@@ -3,7 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest(classes = Main.class)
+@SpringBootTest
 class MainTest {
     @Test
     void contextLoads() {
