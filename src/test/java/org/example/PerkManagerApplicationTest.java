87bf54744cc9d23b4347b291a7d163bc622e9b8d
BigMozz <armin.mozafari12@gmail.com>
2025-11-02 21:40:30 -0500

    CI/CD

--- src/test/java/org/example/PerkManagerApplicationTest.java
+++ src/test/java/org/example/PerkManagerApplicationTest.java
@@ -3,7 +3,7 @@ package org.example;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
+@SpringBootTest(classes = Main.class)
 class PerkManagerApplicationTest {
     @Test
     void contextLoads() {
