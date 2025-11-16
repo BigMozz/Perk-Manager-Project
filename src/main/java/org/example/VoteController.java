ce19ab60ad8eca35ef209f5584b97ef2249967eb
Mofiopefoluwa Olatunji <mofiolatunji@hotmail.com>
2025-11-15 20:03:54 -0500

    added the vote controller class to deal with the upvote and downvote endpoints

--- src/main/java/org/example/VoteController.java
+++ src/main/java/org/example/VoteController.java
@@ -1,11 +1,12 @@
 package org.example;
 
+import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
 import java.util.Optional;
 
-@RestController
+@Controller
 @RequestMapping("/api/votes")
 @CrossOrigin(origins = "*")
 
